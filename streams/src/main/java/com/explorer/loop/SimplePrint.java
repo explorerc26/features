3c47a8804451d5d0305730cc92edf1e6a0b6e6de
explorerc26 <svmc26@gmail.com>
2016-12-06 17:07:28 -0500

    initial

---
+++ streams/src/main/java/com/explorer/loop/SimplePrint.java
@@ -0,0 +1,25 @@
+package com.explorer.loop;
+
+import java.util.Arrays;
+
+public class SimplePrint {
+
+	public static void main(String[] args) {
+		Arrays.asList("a1", "a2", "b1", "c2", "c1")
+	    .parallelStream()
+	    .filter(s -> {
+	        System.out.format("filter: %s [%s]\n",
+	            s, Thread.currentThread().getName());
+	        return true;
+	    })
+	    .map(s -> {
+	        System.out.format("map: %s [%s]\n",
+	            s, Thread.currentThread().getName());
+	        return s.toUpperCase();
+	    })
+	    .forEach(s -> System.out.format("forEach: %s [%s]\n",
+	        s, Thread.currentThread().getName()));
+
+	}
+
+}
