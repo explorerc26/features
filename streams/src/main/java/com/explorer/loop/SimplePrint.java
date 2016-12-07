55a89873028f30931f42fa499ee1c9f1aff0e5da
exmplorerc26-lap <svmc26@gmail.com>
2016-12-07 14:17:13 -0500

    additions

--- streams/src/main/java/com/explorer/loop/SimplePrint.java
+++ streams/src/main/java/com/explorer/loop/SimplePrint.java
@@ -1,11 +1,33 @@
 package com.explorer.loop;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 
 public class SimplePrint {
 
 	public static void main(String[] args) {
-		Arrays.asList("a1", "a2", "b1", "c2", "c1")
+		
+		@SuppressWarnings("serial")
+		List<String> items = new ArrayList<String>() {{
+		    add("item1");add("item2");add("item3");
+		    add("item4");add("item5");add("item6");
+		}};
+		
+		printCollectionSerial(items);
+		printCollectionParallel(items);
+		filterMapPrint(items);
+ 		
+
+
+	}
+	
+	
+	public static void filterMapPrint(Collection<String> items){
+		
+		items
 	    .parallelStream()
 	    .filter(s -> {
 	        System.out.format("filter: %s [%s]\n",
@@ -19,7 +41,26 @@ public class SimplePrint {
 	    })
 	    .forEach(s -> System.out.format("forEach: %s [%s]\n",
 	        s, Thread.currentThread().getName()));
-
+		
+		
+	}
+	
+	public static void printCollectionSerial(Collection<String> items){
+		System.out.println(" -- Going to print the items --");
+		items.stream().forEach(item -> {
+						System.out.format("%s [%s]\n",
+									item, Thread.currentThread().getName());
+									});
+		System.out.println(" -- done printing -- ");
+	}
+	
+	public static void printCollectionParallel(Collection<String> items){
+		System.out.println(" -- Going to print the items --");
+		items.parallelStream().forEach(item -> {
+						System.out.format("%s [%s]\n",
+									item, Thread.currentThread().getName());
+									});
+		System.out.println(" -- done printing -- ");
 	}
 
 }
